dcb202a9d536c763b697107a0a48339b9ca36c71
minsu3007 <minsu3007@gmail.com>
2023-06-12 15:16:23 +0900

    docs: create GyroCell

---
+++ GyroData/GyroCell.swift
@@ -0,0 +1,20 @@
+//
+//  GyroCell.swift
+//  GyroData
+//
+//  Created by 강민수 on 2023/06/12.
+//
+
+import UIKit
+
+class GyroCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
