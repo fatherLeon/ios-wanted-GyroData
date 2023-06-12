c7382bc0005b6bf97b9d7cdce881b82902214a0d
minsu3007 <minsu3007@gmail.com>
2023-06-12 15:00:31 +0900

    feat: Storyboard 삭제 및 기본 뷰 생성

--- GyroData/ViewController.swift
+++ GyroData/ViewController.swift
@@ -11,9 +11,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureNavigationBar()
+        configureView()
     }
+}
 
-
+// MARK: UI
+extension ViewController {
+    private func configureNavigationBar() {
+        self.navigationItem.title = "목록"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "측정", style: .plain, target: self, action: nil)
+    }
+    
+    private func configureView() {
+        self.view.backgroundColor = .systemBackground
+    }
 }
 
